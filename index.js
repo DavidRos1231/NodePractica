d60a9f3b3700f7452361231540a0178f706f6e00
Chris <20233tn137>
2024-05-08 16:42:29 -0600

    Arreglo con url

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ app.get('/', (req, res) => res.send('My first Node API!'));
 
 app.post('/', (req, res) => {
     const word= req.body.message;
+    normalword=word.split('')
+    lowerWord=word.toLowerCase();
+    division=lowerWord.split('');
+
+    for (let index = 0; index < division.length; index++) {
+        if(division[index]!=" "){
+        division[index]=[normalword[index],"https://img.icons8.com/ios/50/sign-language-"+division[index]+".png"]
+        }else{
+            division[index]=[division[index],null]
+        }
+    }
     if(regex.test(word)){
-        res.status(200).send(word.split(''));
+        res.status(200).send(division);
     }else{
         res.status(400).send('Invalid input');
     }
